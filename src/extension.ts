76941268c0fb40ecbdca343f9d8c3aa2471153e5
김근수 <kgs@bi-cns.com>
2023-04-16 03:43:50 +0900

    fixed expand

--- src/extension.ts
+++ src/extension.ts
@@ -2,27 +2,28 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 
 const csscomb = require('csscomb');
-const compress = require('./compress.json');
-const compact = require('./compact.json');
-const expand = require('./expand.json');
+const config: any = {};
+config.compress = require('./compress.json');
+config.compact = require('./compact.json');
+config.expand = require('./expand.json');
 
-const execute = (config: object) => () => {
+const execute = (type: string) => () => {
   // vscode.window.showInformationMessage(type);
   if (!vscode.window.activeTextEditor) {
     return null;
   }
 
   let f = vscode.window.activeTextEditor.document.uri.fsPath;
-  const comb = new csscomb(config);
+  const comb = new csscomb(config[type]);
   comb.processPath(f);
-  vscode.window.showInformationMessage('success csscomb');
+  // vscode.window.showInformationMessage(`success csscomb(${type})`);
 };
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(vscode.commands.registerTextEditorCommand('csscomb.compress', execute(compress)));
-  context.subscriptions.push(vscode.commands.registerTextEditorCommand('csscomb.compact', execute(compact)));
-  context.subscriptions.push(vscode.commands.registerTextEditorCommand('csscomb.expand', execute(expand)));
+  context.subscriptions.push(vscode.commands.registerTextEditorCommand('csscomb.compress', execute('compress')));
+  context.subscriptions.push(vscode.commands.registerTextEditorCommand('csscomb.compact', execute('compact')));
+  context.subscriptions.push(vscode.commands.registerTextEditorCommand('csscomb.expand', execute('expand')));
 }
 
 // This method is called when your extension is deactivated
-// export function deactivate() {}
+export function deactivate() {}
